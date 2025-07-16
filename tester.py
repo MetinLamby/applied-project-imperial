cb533042d563396e39b8880a16f5c600739484a7
Metin Lamby <metin.lamby@outlook.com>
2025-07-16 13:44:10 +0200

    changed requirements

--- tester.py
+++ tester.py
@@ -1,17 +1,1 @@
-'''
-# Load factorZoo.csv
-try:
-    factors = pd.read_csv('factorZoo.csv')
 
-    # Convert 'Date' column to string (if not already)
-    factors['Date'] = factors['Date'].astype(str)
-
-    # Convert to datetime format
-    factors['Date'] = pd.to_datetime(factors['Date'], format='%Y%m%d')
-
-    st.subheader("🗂️ FactorZoo Data (Sample)")
-    st.dataframe(factors.head())
-
-except FileNotFoundError:
-    st.warning("factorZoo.csv file not found. Please upload it to the app directory.")
-'''
